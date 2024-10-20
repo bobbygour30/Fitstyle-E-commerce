37f9a08c12c1ca07082486a399b7fcf06245ff7d
bobbygour30 <bobby30gour@gmail.com>
2024-10-20 14:47:13 +0530

    logo fixed

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
-        <img src={assets.logo} className="w-56" alt="" />
+        <img src={assets.logo} className="w-40" alt="" />
       </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
