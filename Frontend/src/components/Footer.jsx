1f4b4507f15946afade7ec379161486e92ce0c7d
bobbygour30 <bobby30gour@gmail.com>
2024-10-20 14:49:12 +0530

    fixed footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div>
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
-          <img src={assets.logo} className="w-46 mb-5" alt="" />
+          <img src={assets.logo} className="w-40 mb-5" alt="" />
           {/* <p className="w-full md:w-2/3 text-gray-600">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
             iste, harum qui magnam quae itaque voluptas reprehenderit rem? Nobis
